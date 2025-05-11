8c7602de3e3c1d62933579c25a5195cc6464bf7c
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-05-12 03:01:25 +0530

    test001

---
+++ my-app/src/App.jsx
@@ -0,0 +1,58 @@
+import { Button, Card, Label, TextInput } from "flowbite-react";
+import { FaGoogle, FaFacebook } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4">
+      <Card className="w-full max-w-sm p-6 text-center shadow-lg border border-gray-200">
+        <h1 className="text-3xl font-bold mb-2 text-gray-900">Sign Up</h1>
+        <p className="text-sm text-gray-500 mb-6">
+          Already a member?{" "}
+          <a href="#" className="text-blue-600 hover:underline">
+            Log In
+          </a>
+        </p>
+
+        {/* Google Button */}
+        <Button color="light" className="w-full mb-3 border border-gray-300">
+          <div className="flex items-center justify-center gap-2 w-full">
+            <FaGoogle className="text-red-600  size-5" />
+            <span>Sign up with Google</span>
+          </div>
+        </Button>
+
+        {/* Facebook Button */}
+        <Button className="w-full mb-4 bg-blue-600 text-white hover:bg-blue-700">
+          <div className="flex items-center justify-center gap-2 w-full">
+            <FaFacebook className="text-white size-5" />
+            <span>Sign up with Facebook</span>
+          </div>
+        </Button>
+
+        {/* Email & Password Inputs */}
+        <div className="text-left mb-4">
+          <Label htmlFor="email" className="text-sm font-medium text-gray-700">
+            Email
+          </Label>
+          <TextInput id="email" type="email" placeholder="you@example.com" className="mb-3 mt-1" required />
+
+          <Label htmlFor="password" className="text-sm font-medium text-gray-700">
+            Password
+          </Label>
+          <TextInput id="password" type="password" placeholder="••••••••" className="mt-1" required />
+        </div>
+
+
+        {/* Sign up with email */}
+        <button className="w-full bg-green-700 hover:bg-blue-800 text-white font-medium py-2.5 rounded-lg transition-colors">
+          Sign up with email
+        </button>
+
+
+      </Card>
+    </div>
+  );
+}
+
+export default App;
